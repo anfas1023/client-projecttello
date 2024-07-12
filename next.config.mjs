bf087dc09f0282b03d93760b2c70ab8e4b8d8b06
Anfas <anfasmuhammed936@gmail.com>
2024-07-12 12:56:52 +0530

    configaration updated

--- next.config.mjs
+++ next.config.mjs
@@ -1,25 +1,30 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        remotePatterns: [
-            {
-                protocol: 'https',
-                hostname: 'lh3.googleusercontent.com',
-                pathname: '**',
-              },
-              {
-                protocol: 'https',
-                hostname: 'files.edgestore.dev',
-                pathname: '**',
-              },
-              {
-                protocol: 'https',
-                hostname: 'source.unsplash.com',
-                pathname: '**',
-              },
-        ],
-      },
-      reactStrictMode: false,
+  images: {
+      remotePatterns: [
+          {
+              protocol: 'https',
+              hostname: 'lh3.googleusercontent.com',
+              pathname: '**',
+          },
+          {
+              protocol: 'https',
+              hostname: 'files.edgestore.dev',
+              pathname: '**',
+          },
+          {
+              protocol: 'https',
+              hostname: 'source.unsplash.com',
+              pathname: '**',
+          },
+          {
+              protocol: 'https',
+              hostname: 'projecttello.s3.ap-southeast-2.amazonaws.com',
+              pathname: '**',
+          },
+      ],
+  },
+  reactStrictMode: false,
 };
 
 
--- src/components/List-view/AttachementPreview.tsx
+++ src/components/List-view/AttachementPreview.tsx
@@ -1,19 +1,12 @@
-import React, { useState, ChangeEvent, useEffect } from "react";
-
-import { Button } from "@/components/ui/button";
+import React from "react";
+import { HiDocumentArrowUp } from "react-icons/hi2";
+import Image from "next/image";
 import {
   Dialog,
   DialogContent,
-  DialogDescription,
-  DialogFooter,
-  DialogHeader,
-  DialogTitle,
   DialogTrigger,
+  DialogHeader,
 } from "@/components/ui/dialog";
-import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
-import { HiDocumentArrowUp } from "react-icons/hi2";
-import Image from 'next/image'
 
 export function AttachementPreview({
   attachement,
@@ -24,8 +17,6 @@ export function AttachementPreview({
 }) {
   const renderPreview = (attachment: string, orginalName: string) => {
     if (orginalName.endsWith(".pdf")) {
-      // console.log("here");
-
       return (
         <embed
           src={attachment}
@@ -36,9 +27,9 @@ export function AttachementPreview({
       );
     } else {
       return (
-        <Image
+        <Image 
           alt="Preview"
-          src={attachment}
+          src={attachment.startsWith('data:image/') ? attachment : `data:image/png;base64,${attachment}`}
           width={500}
           height={400}
           className="ml-48"
@@ -46,27 +37,18 @@ export function AttachementPreview({
       );
     }
   };
-  console.log("attachment:",attachement,">>",orginalName)
+
   return (
     <Dialog>
       <DialogTrigger asChild>
         <div className="flex gap-2 mt-3  rounded-md cursor-pointer border h-10 w-4/5">
-  <HiDocumentArrowUp />
-  <p className="text-white">{orginalName}</p>
-</div>
+          <HiDocumentArrowUp />
+          <p className="text-white">{orginalName}</p>
+        </div>
       </DialogTrigger>
       <DialogContent className="sm:max-w-[985px] bg-chat">
-        <DialogHeader>
-          {/* <DialogTitle>Preview</DialogTitle> */}
-        </DialogHeader>
+        <DialogHeader>{/* <DialogTitle>Preview</DialogTitle> */}</DialogHeader>
         <div className="grid gap-4 py-4">
-          {/* <img
-            alt="Preview"
-            src={attachement}
-            width={550}
-            height={550}
-            className=""
-          /> */}
           {renderPreview(attachement, orginalName)}
         </div>
       </DialogContent>
