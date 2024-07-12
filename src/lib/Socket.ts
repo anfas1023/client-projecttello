5260137dfbf5fa07939cf5048d1569a3ef4abafd
Anfas <anfasmuhammed936@gmail.com>
2024-07-12 15:08:06 +0530

    socket issue frontend fixed

--- src/lib/Socket.ts
+++ src/lib/Socket.ts
@@ -1,7 +1,6 @@
 // socket.js or socket.ts
 import { io } from 'socket.io-client';
-
-const socket = io(`http://localhost:5000`, {
+const socket = io(`${process.env.NEXT_PUBLIC_BACKEND_URL}`, {
   withCredentials: true,
 });
 
